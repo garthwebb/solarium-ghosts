2124d40d84d20e7d5fe36670f35d6725efd1123d
Garth Webb <garth.webb@gmail.com>
2018-07-04 15:08:48 -0700

    Abstract bus out to the i2c object

--- ghost-writer/PCA9635/device.py
+++ ghost-writer/PCA9635/device.py
@@ -1,16 +1,16 @@
 import constants
 
 
-class Device:
-    bus = None
+class Device(object):
+    i2c = None
     addr = None
 
     # The top 3 bits are for auto increment flags, the lower 5 are for the PWM register address.
     # This sets up an auto increment of the brightness register only, starting at the first register, PWM0
     VALUE_REGISTER = constants.MODE1_AI2 | constants.MODE1_AI0 | constants.REG_PWM0
 
-    def __init__(self, bus, addr):
-        self.bus = bus
+    def __init__(self, i2c, addr):
+        self.i2c = i2c
         self.addr = addr
 
         # Disabled MODE1_SLEEP which is set by default, retains ALLCALL so devices continue to respond to this address
@@ -30,7 +30,7 @@ class Device:
         self.write_register(constants.REG_LEDOUT3, all_pwm)
 
     def write_register(self, register, value):
-        self.bus.write_byte_data(self.addr, register, value)
+        self.i2c.bus.write_byte_data(self.addr, register, value)
 
     def write_values(self, values):
-        self.bus.write_i2c_block_data(self.addr, self.VALUE_REGISTER, values)
+        self.i2c.bus.write_i2c_block_data(self.addr, self.VALUE_REGISTER, values)

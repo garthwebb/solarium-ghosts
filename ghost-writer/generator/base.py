0ec2a48c24026b78e8559c77cc655fbd324c6d06
Garth Webb <garth.webb@gmail.com>
2018-07-04 16:00:31 -0700

    Add test generator

--- ghost-writer/generator/base.py
+++ ghost-writer/generator/base.py
@@ -2,7 +2,7 @@
 
 class Base(object):
 
-    DEFAULT_MAX_VALUE = 1024
+    DEFAULT_MAX_VALUE = 255
 
     def __init__(self, **kwargs):
         self.addresses = []
@@ -11,7 +11,7 @@ class Base(object):
     def set_max_value(self, max_value):
         self.max_value = max_value
 
-    def get_next_frame(self, weights):
+    def get_next_frame(self, addresses):
         """
         Generate the LED values needed for the next frame
         :return:
--- ghost-writer/generator/test.py
+++ ghost-writer/generator/test.py
@@ -2,5 +2,17 @@ from base import Base
 
 
 class Test(Base):
-    def get_next_frame(self, weights):
-        pass
+    def get_next_frame(self, addresses):
+        led_values = []
+
+        for ray in range(8):
+            led_values[ray] = led_values[ray] or []
+            for n in range(16):
+                led_values[ray][n] = led_values[ray][n] or [0, 0, 0]
+                led_values[ray][n][0] += 1
+                led_values[ray][n][1] += 2
+                led_values[ray][n][2] += 3
+
+                led_values[ray][n][0] = led_values[ray][n][0] & 0xFF
+                led_values[ray][n][1] = led_values[ray][n][1] & 0xFF
+                led_values[ray][n][2] = led_values[ray][n][2] & 0xFF
